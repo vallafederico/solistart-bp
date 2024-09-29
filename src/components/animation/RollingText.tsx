ac0f82c0812d7dc02d6eaa28cda5c7aff4f49991
Federico Valla <38297526+vallafederico@users.noreply.github.com>
2024-09-29 12:43:32 +0200

    baseline animaitons

--- src/components/animation/RollingText.tsx
+++ src/components/animation/RollingText.tsx
@@ -3,12 +3,13 @@ import "./RollingText.css";
 export const RollingText = ({
   children,
   class: className,
+  ...rest
 }: {
   children: string;
   class?: string;
 }) => {
   return (
-    <p target={children} animate-hover="roll" class={className}>
+    <p {...rest} target={children} animate-hover="roll" class={className}>
       <span>{children}</span>
     </p>
   );
